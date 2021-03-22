92890e1c258d0b0af2a0609f4d018fa01aa8bce9
Peter Hua <diskrooms@gmail.com>
2021-03-22 22:13:03 +0800

    first

---
+++ src/api/app.js
@@ -0,0 +1,33 @@
+import request from '@/utils/request'
+
+export function getList(data) {
+  return request({
+    url: '/app/list',
+    method: 'post',
+    data
+  })
+}
+//应用添加
+export function requestAdd(data) {
+  return request({
+    url: '/app/add',
+    method: 'post',
+    data
+  })
+}
+//应用轮询
+export function requestEvent(data) {
+  return request({
+    url: '/app/event_poll',
+    method: 'post',
+    data
+  })
+}
+//应用删除
+export function requestDelete(data) {
+  return request({
+    url: '/app/del',
+    method: 'post',
+    data
+  })
+}
